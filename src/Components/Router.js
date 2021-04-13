59424176a64cc9b39644099041a55f21b3353971
kajj8808 <71279997+kajj8808@users.noreply.github.com>
2021-04-14 03:45:48 +0900

    nomflix

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Header from "Components/Header";
 import Movie from "Routes/Movie";
 import TV from "Routes/TV";
